fb51057ec56398712e4f7bc057992404bec69f52
Adarsh kumar mishra <theakm037@gmail.com>
2025-09-14 16:52:12 +0530

    changes done to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { useState } from "react";
-import Hero from "@/components/Hero";
-
+import HeroShop from "@/components/Hero";
 
 export default function HomePage() {
-  const [showShop, setShowShop] = useState(false);
-
   return (
     <div className="relative w-full h-full">
-      {!showShop && <Hero setShowShop={setShowShop} />}
-      {showShop && <Shop />}
+      <HeroShop />
     </div>
   );
 }
